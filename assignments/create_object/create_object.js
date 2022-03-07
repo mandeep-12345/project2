ac719b2e6851a4071985ae5144593dfeb321f19b
mandeep-12345 <mandiip.dass1997@gmail.com>
2022-03-07 12:35:21 +0530

    assignment

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,4 +1,16 @@
 function CreateObject(arr) {
+    
+    const obj={}
+    for(let i=0;i<arr.length-1;i++){
+        if(i%2===0){
+            obj[arr[i]]=arr[i+1]
+        }
+    }
+    return(obj)
+
+    
+
+
     // Write your code here
 }
 
